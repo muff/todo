ac9ec806cfdfea7de4230852f29e9fe68baae6c5
Eivind <eivindingebrigtsen@gmail.com>
2011-11-06 17:41:43 +0100

    ready for pres

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 #jsTodo
+a very small todo app in javascript,
+- Jasmine-Behaviour driven development
+- jQuery for DOM manipulation
+- Handlebars for templating
+- Twitter Bootstrap for interface
+
--- spec.js
+++ spec.js
@@ -5,8 +5,12 @@ describe("todo app", function(){
       
   it('should have an input to add todo items', function(){
     expect(input.length).toBe(1);
+    expect(input.prop('nodeName')).toBe('INPUT');
   });
   it('should have a list for todo items', function(){
     expect(list.length).toBe(1);
+    expect(list.prop('nodeName')).toBe('UL');
   });
+  
+  
 });
